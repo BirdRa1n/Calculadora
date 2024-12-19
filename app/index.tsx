edc82ef26ba1901d7d13358c3b79069bf8625b97
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2024-12-19 16:32:48 -0300

    Update index.tsx
    - Refatoração na lógica de como os botões são tratados na função handleButtonPress.
    - Adição de condições para prevenir operadores consecutivos.
    - Melhoria no controle de operadores e substituição correta dos mesmos.
    - Ajustes na estrutura do componente de interface, como a renderização de botões e o layout da tela.

--- app/index.tsx
+++ app/index.tsx
@@ -4,65 +4,65 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
 
 const HomeScreen = () => {
-    // Estado que armazena os números e expressão atual do usuário
     const [numbers, setNumbers] = useState<string>('');
-    // Define o esquema de cores com base nas configurações do sistema (claro/escuro)
     const colorScheme = useColorScheme();
 
-    // Matriz que define os botões exibidos na calculadora
     const buttons = [
-        ['7', '8', '9', '⌫'], // ⌫ representa o símbolo de apagar
+        ['7', '8', '9', '⌫'],
         ['4', '5', '6', '-'],
         ['1', '2', '3', '+'],
         ['/', '0', '*', '='],
-        ['CE', ','], // CE limpa a expressão, ',' adiciona um ponto decimal
+        ['CE', ','],
     ];
 
-    // Função para calcular o resultado da expressão atual
     const calcularResultado = () => {
         try {
-            // Avaliar a expressão usando a biblioteca mathjs
             const resultado = evaluate(numbers);
-
-            // Atualiza o estado com o resultado convertido para string
             setNumbers(resultado.toString());
         } catch (error) {
-            // Caso ocorra erro na avaliação, exibe mensagem de erro
             setNumbers("");
         }
     };
 
-    // Lida com os eventos de clique nos botões
     const handleButtonPress = (button: string) => {
-        switch (button) {
-            case '=':
-                // Chama a função para calcular o resultado
-                calcularResultado();
-                break;
+        console.log(`handleButtonPress: button = ${button}, numbers = ${numbers}`);
+        // Verifica se o caractere final da string de numeros é um operador
+        const lastChar = numbers.slice(-1);
+        const isOperator = (char: string) => ['+', '-', '*', '/'].includes(char);
 
-            case '⌫':
-                // Remove o último caractere da expressão
-                setNumbers(numbers.slice(0, -1));
-                break;
-
-            case ',':
-                // Substitui a vírgula por ponto para decimal
-                setNumbers(numbers + '.');
-                break;
-
-            case 'CE':
-                // Limpa a expressão atual
-                setNumbers('');
-                break;
-
-            default:
-                // Evita operadores duplicados consecutivos
-                if (numbers.slice(-1) === button && ['+', '-', '*', '/'].includes(button)) {
-                    return;
-                }
-
-                // Adiciona o botão pressionado à expressão
-                setNumbers(numbers + button);
+        // Se o botao for um operador, o tratamento é diferente
+        if (isOperator(button)) {
+            console.log(`handleButtonPress: isOperator(button) = ${isOperator(button)}`);
+            // Se o caractere final eh um operador, substitui ele pelo novo operador
+            setNumbers(isOperator(lastChar) ? numbers.slice(0, -1) + button : numbers + button);
+        } else {
+            // Tratamento para cada botao
+            switch (button) {
+                case '=':
+                    console.log(`handleButtonPress: button = ${button}`);
+                    // Calcula o resultado da expressao
+                    calcularResultado();
+                    break;
+                case ' ':
+                    console.log(`handleButtonPress: button = ${button}`);
+                    // Remove o caractere final da string de numeros
+                    setNumbers(numbers.slice(0, -1));
+                    break;
+                case ',':
+                    console.log(`handleButtonPress: button = ${button}`);
+                    // Substitui a virgula (ponto decimal) pelo ponto
+                    setNumbers(numbers + '.');
+                    break;
+                case 'CE':
+                    console.log(`handleButtonPress: button = ${button}`);
+                    // Limpa a string de numeros
+                    setNumbers('');
+                    break;
+                default:
+                    console.log(`handleButtonPress: button = ${button}`);
+                    // Adiciona o botao pressionado ao final da string de numeros
+                    setNumbers(numbers + button);
+            }
         }
     };
 
@@ -71,7 +71,6 @@ const HomeScreen = () => {
     return (
         <View style={{ flex: 1, height: '100%' }}>
             <SafeAreaView>
-                {/* Tela de exibição da expressão e resultado */}
                 <View style={{ height: height / 4, alignItems: 'flex-end', justifyContent: 'flex-end', padding: 10 }}>
                     <Text
                         numberOfLines={1}
@@ -81,7 +80,6 @@ const HomeScreen = () => {
                     </Text>
                 </View>
 
-                {/* Renderização dos botões */}
                 {buttons.map((row, index) => (
                     <View key={index} style={{ flexDirection: 'row', margin: 10, justifyContent: 'space-between' }}>
                         {row.map((button, buttonIndex) => (
@@ -105,6 +103,6 @@ const HomeScreen = () => {
             </SafeAreaView>
         </View>
     );
-}
+};
 
 export default HomeScreen;
